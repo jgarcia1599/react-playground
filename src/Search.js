c4eb1209447a14cb31c0b9e71e0ce17fe3c9e77d
Junior Francisco Garcia Ayala <junior.f.garcia99@gmail.com>
2020-05-06 12:23:40 +0400

    added dual display functionality

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import'bootstrap/dist/css/bootstrap.min.css';
+import $ from'jquery';
+import Popper from'popper.js';
+import'bootstrap/dist/js/bootstrap.bundle.min';
+
 import "./css/Search.css";
 import './css/map.css';
 import { Slider, Rail, Handles, Tracks, Ticks } from "react-compound-slider";
@@ -43,11 +48,12 @@ class Search extends Component {
     <BoatGrid  name="Boat_6" date={1900} lat_long={[27,56]} source={1}/>,
     <BoatGrid  name="Boat_7" date={1850} lat_long={[27,54]} source={2}/>
     ],
-    map_data:[]
+    map_data:[],
+    display : "both"
 
   };
 
-  handleOnChange = event => {
+  searchOnChange = event => {
     this.setState({ searchValue: event.target.value });
     if (this.state.searchValue === ""){
       this.setState({
@@ -86,7 +92,7 @@ class Search extends Component {
     }
   }
 
-  onUpdate = event =>{
+  slideronUpdate = event =>{
     var min_date = event[0];
     var max_date = event[1];
 
@@ -104,13 +110,37 @@ class Search extends Component {
     })
 
   }
-  render() {
-    //Variable to send boat components to Leaflet Map
-    var send_to_map = [];
-    return (
-      <div id="main">
+
+  chooseDisplay = (display,type) =>{
+
+    if (type === "map"){
+      console.log("map")
+      this.setState({
+        display:"map"
+      })
+
+    }
+    else if (type === "grid"){
+      console.log("grid")
+      this.setState({
+        display:"grid"
+      })
+
+
+    }
+    else if (type === "both"){
+      console.log("both")
+      this.setState({
+        display:"both"
+      })
+    }
+  }
+
+  renderSearch(){
+    return(
+        <div>
         <h1>Dhakira's 3D[how] Database</h1>
-        <input name="text" type="text" placeholder="Search" onChange={event => this.handleOnChange(event)} value={this.state.searchValue}/>
+        <input name="text" type="text" placeholder="Search" onChange={event => this.searchOnChange(event)} value={this.state.searchValue}/>
         <p>Time Period</p>
         {/* <input value="0" min="0" max="2000" step="100" type="range"/> */}
         <Slider
@@ -118,7 +148,7 @@ class Search extends Component {
           step={1}
           domain={domain}
           rootStyle={sliderStyle}
-          onUpdate={this.onUpdate}
+          onUpdate={this.slideronUpdate}
           onChange={this.onChange}
           values={defaultValues}
         >
@@ -165,31 +195,109 @@ class Search extends Component {
         </Slider>
       <br/>
       <br/>
-      {this.state.boat_results ? (
-        <div id="meals-container">
-            {this.state.boat_results.map((boat,index) => {
-              var boat_date = boat.props.date
-              if(boat_date>this.state.min_date && boat_date< this.state.max_date ){
-                console.log("Render this boat");
-                send_to_map.push(boat);
-                return (
-                  <React.Fragment key={index}>
-                      {boat}
-                  </React.Fragment>)
-              }
-            })}
-          </div>
-
-        ) : (
-          <p>No results!</p>
-        )}
-
-    <LeafletMap map_data={send_to_map}/>
-    </div> 
+      {/* <div className="btn-group">
+        <button type="button" className="btn btn-primary">Map</button>
+        <button type="button" className="btn btn-primary">Grid</button>
+        <button type="button" className="btn btn-primary">Both</button>
+    </div> */}
+        <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Display
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" onClick={ display => this.chooseDisplay(display,"map")}>Map Display</a>
+          <a class="dropdown-item" onClick={ display => this.chooseDisplay(display,"grid")}>Grid Display</a>
+          <a class="dropdown-item" onClick={ display => this.chooseDisplay(display,"both")}>Dual Display</a>
+        </div>
+      </div>
+      </div>
 
 
+    )
+  };
+  render() {
+    // //Variable to send boat components to Leaflet Map
+    var send_to_map = [];
+    if (this.state.display === "both"){
+      return (
+        <div id="main">
+          {this.renderSearch()}
+        {this.state.boat_results ? (
+          <div id="meals-container">
+              {this.state.boat_results.map((boat,index) => {
+                var boat_date = boat.props.date
+                if(boat_date>this.state.min_date && boat_date< this.state.max_date ){
+                  console.log("Render this boat");
+                  send_to_map.push(boat);
+                  return (
+                    <React.Fragment key={index}>
+                        {boat}
+                    </React.Fragment>)
+                }
+              })}
+            </div>
+  
+          ) : (
+            <p>No results!</p>
+          )}
+  
+      <LeafletMap map_data={send_to_map}/>
+      </div> 
+      );
 
-    );
+    }
+    else if (this.state.display === "map"){
+      return (
+        <div id="main">
+          {this.renderSearch()}
+        {this.state.boat_results ? (
+          <div id="meals-container">
+              {this.state.boat_results.map((boat,index) => {
+                var boat_date = boat.props.date
+                if(boat_date>this.state.min_date && boat_date< this.state.max_date ){
+                  console.log("Render this boat");
+                  send_to_map.push(boat);
+                }
+              })}
+            </div>
+  
+          ) : (
+            <p>No results!</p>
+          )}
+  
+      <LeafletMap map_data={send_to_map}/>
+      </div> 
+      );
+    }
+
+    else if (this.state.display === "grid") {
+      console.log("jdjdjdjdj")
+      return (
+        <div id="main">
+          {this.renderSearch()}
+        {this.state.boat_results ? (
+          <div id="meals-container">
+              {this.state.boat_results.map((boat,index) => {
+                var boat_date = boat.props.date
+                if(boat_date>this.state.min_date && boat_date< this.state.max_date ){
+                  console.log("Render this boat");
+                  send_to_map.push(boat);
+                  return (
+                    <React.Fragment key={index}>
+                        {boat}
+                    </React.Fragment>)
+                }
+              })}
+            </div>
+  
+          ) : (
+            <p>No results!</p>
+          )}
+      </div> 
+      );
+
+    }
+
 
   }
 }
